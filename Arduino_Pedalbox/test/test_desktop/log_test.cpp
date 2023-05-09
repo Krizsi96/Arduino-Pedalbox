defcefa4b83f41432df8f08643046feb0590967e
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-05-09 19:30:51 +0200

    refactor:  test layout

--- Arduino_Pedalbox/test/test_desktop/log_test.cpp
+++ Arduino_Pedalbox/test/test_desktop/log_test.cpp
@@ -7,12 +7,17 @@
 using ::testing::_;
 
 TEST(LogTest, TC149LogMessageStructure) {
-  // Setup
+  // Preconditions
   ArduinoSerialMock serial_mock;
   ArduinoTimeInterface time;
   TimeLibMock time_lib;
   Log log(&time, &serial_mock, &time_lib);
 
+  EXPECT_CALL(time_lib, get_hour()).WillOnce(::testing::Return(0));
+  EXPECT_CALL(time_lib, get_minute()).WillOnce(::testing::Return(0));
+  EXPECT_CALL(time_lib, get_second()).WillOnce(::testing::Return(0));
+
+  // Expected behavior
   char time_str[] = "00:00:00.000";
   char log_type[] = "INFO";
   char file[] = "test/test_desktop/log_test.cpp";
@@ -21,12 +26,9 @@ TEST(LogTest, TC149LogMessageStructure) {
   char buffer[100];
   sprintf(buffer, "%s %s %s:%d: %s\n", time_str, log_type, file, line, message);
   const std::string expected = buffer;
-
-  EXPECT_CALL(time_lib, get_hour()).WillOnce(::testing::Return(0));
-  EXPECT_CALL(time_lib, get_minute()).WillOnce(::testing::Return(0));
-  EXPECT_CALL(time_lib, get_second()).WillOnce(::testing::Return(0));
   EXPECT_CALL(serial_mock, print(expected));
 
+  // Action
   log.createLog(Log::kInfo, file, line, message);
 
   ::testing::Mock::VerifyAndClearExpectations(&serial_mock);
