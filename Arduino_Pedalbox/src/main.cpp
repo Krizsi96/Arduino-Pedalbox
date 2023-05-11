78c1554b55a9d5f446c46b6090c4f790baebb1b6
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-05-11 21:46:23 +0200

    refactor: clean main.cpp

--- Arduino_Pedalbox/src/main.cpp
+++ Arduino_Pedalbox/src/main.cpp
@@ -2,15 +2,10 @@
 
 #include "log.h"
 
-Log logging;
-
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  logging.updateTime();
-  logging.digitalClockDisplay(10);
 }
