7d645a3653f354792aa7fe6ad47a3da8d0c24f62
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-05-14 12:16:42 +0200

    test: add tests for arduino wrapper

---
+++ Arduino_Pedalbox/test/test_target/ArduinoWrapper_test.hpp
@@ -0,0 +1,60 @@
+#include <ArduinoWrapper.h>
+#include <unity.h>
+
+#include <ArduinoWrapper.cpp>
+
+void test_if_hour_is_set_correctly(void) {
+  ArduinoWrapper arduino;
+  arduino.Time.SetTime(1, 0, 0, 1, 1, 1970);
+  TEST_ASSERT_EQUAL(1, arduino.Time.get_hour());
+}
+
+void test_if_minute_is_set_correctly(void) {
+  ArduinoWrapper arduino;
+  arduino.Time.SetTime(0, 1, 0, 1, 1, 1970);
+  TEST_ASSERT_EQUAL(1, arduino.Time.get_minute());
+}
+
+void test_if_second_is_set_correctly(void) {
+  ArduinoWrapper arduino;
+  arduino.Time.SetTime(0, 0, 1, 1, 1, 1970);
+  TEST_ASSERT_EQUAL(1, arduino.Time.get_second());
+}
+
+void test_if_hour_is_set_correctly_with_unix_time(void) {
+  ArduinoWrapper arduino;
+  arduino.Time.SetTime(1356998400);
+  TEST_ASSERT_EQUAL(0, arduino.Time.get_hour());
+}
+
+void test_if_minute_is_set_correctly_with_unix_time(void) {
+  ArduinoWrapper arduino;
+  arduino.Time.SetTime(1356998400);
+  TEST_ASSERT_EQUAL(0, arduino.Time.get_minute());
+}
+
+void test_if_second_is_set_correctly_with_unix_time(void) {
+  ArduinoWrapper arduino;
+  arduino.Time.SetTime(1356998400);
+  TEST_ASSERT_EQUAL(0, arduino.Time.get_second());
+}
+
+void test_if_time_is_updated(void) {
+  ArduinoWrapper arduino;
+  arduino.Time.SetTime(1356998400);
+  uint16_t previous_millisecond = arduino.Time.get_millisecond();
+  for (int i = 0; i < 1000; i++) {
+  }
+  arduino.Time.update();
+  TEST_ASSERT_NOT_EQUAL(previous_millisecond, arduino.Time.get_millisecond());
+}
+
+void run_arduino_wrapper_test(void) {
+  RUN_TEST(test_if_hour_is_set_correctly);
+  RUN_TEST(test_if_minute_is_set_correctly);
+  RUN_TEST(test_if_second_is_set_correctly);
+  RUN_TEST(test_if_hour_is_set_correctly_with_unix_time);
+  RUN_TEST(test_if_minute_is_set_correctly_with_unix_time);
+  RUN_TEST(test_if_second_is_set_correctly_with_unix_time);
+  RUN_TEST(test_if_time_is_updated);
+}
--- Arduino_Pedalbox/test/test_target/test_main.cpp
+++ Arduino_Pedalbox/test/test_target/test_main.cpp
@@ -1,50 +1,7 @@
-#include <Arduino.h>
+#include <ArduinoWrapper.h>
 #include <unity.h>
 
-#include "TimeLib_interface_test.hpp"
-
-// #include "log.h"
-
-#define BUTTON_PIN 5
-
-#ifdef MANUAL_TEST
-
-Log logging;
-long time = 0;
-long previous_time = 0;
-bool button_pressed = false;
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(BUTTON_PIN, INPUT);
-  digitalWrite(BUTTON_PIN, LOW);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  logging.updateTime();
-
-  time = millis();
-  if ((time - previous_time) > 1000) {
-    previous_time = time;
-    logging.createLog(logging.kInfo, __FILE__, __LINE__, "one second passed");
-  }
-
-  if (digitalRead(BUTTON_PIN) == HIGH) {
-    if (!button_pressed) {
-      logging.createLog(logging.kDebug, __FILE__, __LINE__, "button pressed");
-      button_pressed = true;
-    }
-  } else {
-    if (button_pressed) {
-      logging.createLog(logging.kDebug, __FILE__, __LINE__, "button released");
-      button_pressed = false;
-    }
-  }
-}
-
-#else
+#include "ArduinoWrapper_test.hpp"
 
 void setup() {
   Serial.begin(9600);
@@ -52,10 +9,8 @@ void setup() {
     ;  // wait for serial port to connect. Needed for native USB port only
   }
   UNITY_BEGIN();
-  run_time_lib_interface_test();
+  run_arduino_wrapper_test();
   UNITY_END();
 }
 
 void loop() { delay(1000); }
-
-#endif  // MANUAL_TEST
