4563fbf2e826db01d78008dbde196034964efbb9
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-05-14 12:23:55 +0200

    demo code for logging

--- Arduino_Pedalbox/src/main.cpp
+++ Arduino_Pedalbox/src/main.cpp
@@ -1,11 +1,40 @@
-#include <Arduino.h>
+#include "ArduinoWrapper.h"
+#include "log.hpp"
 
-#include "log.h"
+#define BUTTON_PIN 5
+
+ArduinoWrapper arduino;
+Log logging(&arduino.Serial, &arduino.Time);
+long time = 0;
+long previous_time = 0;
+bool button_pressed = false;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(BUTTON_PIN, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  arduino.Time.update();
+
+  time = millis();
+  if ((time - previous_time) > 1000) {
+    previous_time = time;
+    logging.createLog(logging.kInfo, __FILE__, __LINE__, "one second passed");
+  }
+
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    if (!button_pressed) {
+      logging.createLog(logging.kDebug, __FILE__, __LINE__, "button pressed");
+      button_pressed = true;
+    }
+  } else {
+    if (button_pressed) {
+      logging.createLog(logging.kDebug, __FILE__, __LINE__, "button released");
+      button_pressed = false;
+    }
+  }
 }
