a80798e6bc4322d6240d67298bc765ec5b93bc79
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-08-20 19:23:07 +0100

    feat: delete previous test code and add load cell reading

--- Arduino_Pedalbox/src/main.cpp
+++ Arduino_Pedalbox/src/main.cpp
@@ -1,40 +1,25 @@
 #include "ArduinoWrapper.hpp"
-#include "Log.hpp"
+#include "LoadCell.hpp"
 
-#define BUTTON_PIN 5
+#define HX711_DOUT 10
+#define HX711_SCK 9
 
 ArduinoWrapper arduino;
-Log logging(&arduino.Time, &arduino.Serial);
 long time = 0;
 long previous_time = 0;
-bool button_pressed = false;
+
+LoadCell load_cell(HX711_SCK, HX711_DOUT);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(BUTTON_PIN, INPUT);
-  digitalWrite(BUTTON_PIN, LOW);
+  load_cell.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  arduino.Time.update();
-
   time = millis();
   if ((time - previous_time) > 1000) {
     previous_time = time;
-    logging.createLog(logging.kInfo, __FILE__, __LINE__, "one second passed");
-  }
-
-  if (digitalRead(BUTTON_PIN) == HIGH) {
-    if (!button_pressed) {
-      logging.createLog(logging.kDebug, __FILE__, __LINE__, "button pressed");
-      button_pressed = true;
-    }
-  } else {
-    if (button_pressed) {
-      logging.createLog(logging.kDebug, __FILE__, __LINE__, "button released");
-      button_pressed = false;
-    }
+    int32_t load_cell_reading = load_cell.getReadingValue();
+    Serial.println(load_cell_reading);
   }
 }
