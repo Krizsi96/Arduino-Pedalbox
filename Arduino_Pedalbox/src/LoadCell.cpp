6b77448997ce14c07f7b5d55a6f34fe9a9d36a4b
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-08-20 14:04:41 +0100

    refactor: add empty functions

---
+++ Arduino_Pedalbox/src/LoadCell.cpp
@@ -0,0 +1,13 @@
+#include "LoadCell.hpp"
+
+LoadCell::LoadCell() {}
+
+LoadCell::~LoadCell() {}
+
+int32_t LoadCell::getRawValue() { return 0; }
+
+void LoadCell::applyFilter() {}
+
+void LoadCell::applyOffset() {}
+
+void LoadCell::updateFilterParameter() {}
---
+++ Arduino_Pedalbox/src/Pedal.cpp
@@ -0,0 +1,11 @@
+#include "Pedal.hpp"
+
+Pedal::Pedal() {}
+
+Pedal::~Pedal() {}
+
+int32_t Pedal::readValue() { return 0; }
+
+void Pedal::setOffset() {}
+
+void Pedal::setFilter() {}
