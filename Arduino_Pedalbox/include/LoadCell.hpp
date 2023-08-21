97c2e445529c716995574b288b26e3434aabda98
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-08-21 20:14:19 +0100

    refactor: move begin() function into constructor

--- Arduino_Pedalbox/include/LoadCell.hpp
+++ Arduino_Pedalbox/include/LoadCell.hpp
@@ -9,14 +9,11 @@ class LoadCell : public SensorInterface {
  public:
   LoadCell(byte serial_clock_pin_init, byte serial_data_pin_init);
   ~LoadCell() override = default;
-  void begin();
   int32_t getReadingValue() override;
   void updateOffset(int32_t new_offset) override;
 
  private:
   HX711 load_cell_;
-  byte serial_clock_pin_;
-  byte serial_data_pin_;
 };
 
 #endif  //_LOADCELL_HPP_
--- Arduino_Pedalbox/src/LoadCell.cpp
+++ Arduino_Pedalbox/src/LoadCell.cpp
@@ -1,11 +1,7 @@
 #include "LoadCell.hpp"
 
-LoadCell::LoadCell(byte serial_clock_pin_init, byte serial_data_pin_init)
-    : serial_clock_pin_(serial_clock_pin_init),
-      serial_data_pin_(serial_data_pin_init) {}
-
-void LoadCell::begin() {
-  load_cell_.begin(serial_data_pin_, serial_clock_pin_);
+LoadCell::LoadCell(byte serial_clock_pin_init, byte serial_data_pin_init) {
+  load_cell_.begin(serial_data_pin_init, serial_clock_pin_init);
 }
 
 int32_t LoadCell::getReadingValue() {
