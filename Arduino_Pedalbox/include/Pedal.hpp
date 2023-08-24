24d6d421cec82b35eaf795211f80a4c13e4133de
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-08-24 19:04:30 +0100

    feat: add readValue and setOffset functions definitions

--- Arduino_Pedalbox/include/Pedal.hpp
+++ Arduino_Pedalbox/include/Pedal.hpp
@@ -8,10 +8,12 @@
 class Pedal {
  public:
   int32_t readValue();
-  void setOffset();
-  void setFilter();
-  Pedal();
+  void setOffset(int32_t new_offset);
+  Pedal(SensorInterface* sensor);
   ~Pedal();
+
+ private:
+  SensorInterface* sensor_;
 };
 
 #endif  //_PEDAL_HPP_
--- Arduino_Pedalbox/src/Pedal.cpp
+++ Arduino_Pedalbox/src/Pedal.cpp
@@ -1,11 +1,9 @@
 #include "Pedal.hpp"
 
-Pedal::Pedal() {}
+Pedal::Pedal(SensorInterface* sensor) : sensor_(sensor) {}
 
 Pedal::~Pedal() {}
 
-int32_t Pedal::readValue() { return 0; }
+int32_t Pedal::readValue() { return sensor_->getReadingValue(); }
 
-void Pedal::setOffset() {}
-
-void Pedal::setFilter() {}
+void Pedal::setOffset(int32_t new_offset) { sensor_->setOffset(new_offset); }
