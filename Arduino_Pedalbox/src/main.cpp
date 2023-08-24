3cb5006fd793378e01802ee18edf6b1eaa1eeb1d
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-08-24 19:04:54 +0100

    refactor: use Pedal class in main.cpp

--- Arduino_Pedalbox/src/main.cpp
+++ Arduino_Pedalbox/src/main.cpp
@@ -1,5 +1,6 @@
 #include "ArduinoWrapper.hpp"
 #include "LoadCell.hpp"
+#include "Pedal.hpp"
 #include "SensorConfig.h"
 
 ArduinoWrapper arduino;
@@ -7,17 +8,18 @@ long time = 0;
 long previous_time = 0;
 
 LoadCell load_cell(BRAKE_PEDAL_SCK, BRAKE_PEDAL_DOUT);
+Pedal brake_pedal(&load_cell);
 
 void setup() {
   Serial.begin(9600);
-  load_cell.setOffset(BRAKE_PEDAL_OFFSET);
+  brake_pedal.setOffset(BRAKE_PEDAL_OFFSET);
 }
 
 void loop() {
   time = millis();
   if ((time - previous_time) > 1000) {
     previous_time = time;
-    int32_t load_cell_reading = load_cell.getReadingValue();
-    Serial.println(load_cell_reading);
+    int32_t brake_pedal_reading = brake_pedal.readValue();
+    Serial.println(brake_pedal_reading);
   }
 }
