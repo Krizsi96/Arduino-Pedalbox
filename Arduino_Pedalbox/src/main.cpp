8541d2238ccb59be1673ab453fed12136bf3801e
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-09-04 18:55:54 +0100

    FEAT:: create throttle pedal in main function

--- Arduino_Pedalbox/src/main.cpp
+++ Arduino_Pedalbox/src/main.cpp
@@ -1,6 +1,7 @@
 #include "ArduinoWrapper.hpp"
 #include "LoadCell.hpp"
 #include "Pedal.hpp"
+#include "Potmeter.hpp"
 #include "SensorConfig.h"
 
 ArduinoWrapper arduino;
@@ -8,11 +9,14 @@ long time = 0;
 long previous_time = 0;
 
 LoadCell load_cell(BRAKE_PEDAL_SCK, BRAKE_PEDAL_DOUT);
+Potmeter potmeter(THROTTLE_PEDAL_PIN);
 Pedal brake_pedal(&load_cell);
+Pedal throttle_pedal(&potmeter);
 
 void setup() {
   Serial.begin(9600);
   brake_pedal.setOffset(BRAKE_PEDAL_OFFSET);
+  throttle_pedal.setOffset(THROTTLE_PEDAL_OFFSET);
 }
 
 void loop() {
@@ -20,6 +24,10 @@ void loop() {
   if ((time - previous_time) > 1000) {
     previous_time = time;
     int32_t brake_pedal_reading = brake_pedal.readValue();
-    Serial.println(brake_pedal_reading);
+    int32_t throttle_pedal_reading = throttle_pedal.readValue();
+    Serial.print("Brake:\t");
+    Serial.print(brake_pedal_reading);
+    Serial.print("\t Throttle:\t");
+    Serial.println(throttle_pedal_reading);
   }
 }
