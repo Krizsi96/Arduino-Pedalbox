ece2d1de4eafa4c3e54d8c82abf48e535bbed622
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-09-04 18:55:22 +0100

    FEAT:: add definitions to class methods

---
+++ Arduino_Pedalbox/src/Potmeter.cpp
@@ -0,0 +1,11 @@
+#include "Potmeter.hpp"
+
+#include <Arduino.h>
+
+Potmeter::Potmeter(uint8_t analog_pin_init) : analog_pin_(analog_pin_init) {}
+
+int32_t Potmeter::getReadingValue() {
+  return analogRead(analog_pin_) + offset_;
+}
+
+void Potmeter::setOffset(int32_t new_offset) { offset_ = new_offset; }
