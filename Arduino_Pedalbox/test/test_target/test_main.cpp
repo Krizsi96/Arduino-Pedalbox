da0a554403ad33aa1fdf06e3508c9e3cf5f62acb
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-09-05 18:02:09 +0100

    FEAT:: use lcd screen to display info while running tests

--- Arduino_Pedalbox/test/test_target/test_main.cpp
+++ Arduino_Pedalbox/test/test_target/test_main.cpp
@@ -1,17 +1,66 @@
+#include <LiquidCrystal.h>
 #include <unity.h>
 
 #include <ArduinoWrapper.hpp>
 
 #include "ArduinoWrapper_test.hpp"
 
+#define LCD_RS 8
+#define LCD_EN 9
+#define LCD_D4 4
+#define LCD_D5 5
+#define LCD_D6 6
+#define LCD_D7 7
+
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
+void printTestInfo(uint8_t number, const char* test_name) {
+  lcd.clear();
+  lcd.print("Test ");
+  lcd.print(number);
+  lcd.print("/7");
+  lcd.setCursor(0, 1);
+  lcd.print(test_name);
+}
+
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
+    {
+    }  // wait for serial port to connect. Needed for native USB port only
   }
+  lcd.begin(16, 2);
+  lcd.print("Starting tests...");
   UNITY_BEGIN();
-  run_arduino_wrapper_test();
+
+  lcd.setCursor(0, 1);
+  lcd.print("ArduinoWrapper tests");
+
+  printTestInfo(1, "test_if_hour_is_set_correctly");
+  RUN_TEST(test_if_hour_is_set_correctly);
+
+  printTestInfo(2, "test_if_minute_is_set_correctly");
+  RUN_TEST(test_if_minute_is_set_correctly);
+
+  printTestInfo(3, "test_if_second_is_set_correctly");
+  RUN_TEST(test_if_second_is_set_correctly);
+
+  printTestInfo(4, "test_if_hour_is_set_correctly_with_unix_time");
+  RUN_TEST(test_if_hour_is_set_correctly_with_unix_time);
+
+  printTestInfo(5, "test_if_minute_is_set_correctly_with_unix_time");
+  RUN_TEST(test_if_minute_is_set_correctly_with_unix_time);
+
+  printTestInfo(6, "test_if_second_is_set_correctly_with_unix_time");
+  RUN_TEST(test_if_second_is_set_correctly_with_unix_time);
+
+  printTestInfo(7, "test_if_time_is_updated");
+  RUN_TEST(test_if_time_is_updated);
+
   UNITY_END();
+
+  lcd.clear();
+  lcd.print("Tests finished!");
 }
 
 void loop() { delay(1000); }
