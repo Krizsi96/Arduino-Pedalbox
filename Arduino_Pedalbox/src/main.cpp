9904f8cb8c174bf8aeecd5fcac33ba5cd716b6ab
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-09-06 19:12:37 +0100

    FEAT:: use arduino-timer for the cyclic tasks

--- Arduino_Pedalbox/src/main.cpp
+++ Arduino_Pedalbox/src/main.cpp
@@ -1,3 +1,5 @@
+#include <arduino-timer.h>
+
 #include "ArduinoWrapper.hpp"
 #include "LoadCell.hpp"
 #include "Pedal.hpp"
@@ -5,8 +7,8 @@
 #include "SensorConfig.h"
 
 ArduinoWrapper arduino;
-long time = 0;
-long previous_time = 0;
+
+auto timer = timer_create_default();
 
 LoadCell brake_load_cell(BRAKE_PEDAL_SCK, BRAKE_PEDAL_DOUT);
 Potmeter throttle_potmeter(THROTTLE_PEDAL_PIN);
@@ -16,25 +18,27 @@ Pedal brake_pedal(&brake_load_cell);
 Pedal throttle_pedal(&throttle_potmeter);
 Pedal clutch_pedal(&clutch_potmeter);
 
+bool showPedalReadings(void *);
+
 void setup() {
   Serial.begin(9600);
   brake_pedal.setOffset(BRAKE_PEDAL_OFFSET);
   throttle_pedal.setOffset(THROTTLE_PEDAL_OFFSET);
   clutch_pedal.setOffset(CLUTCH_PEDAL_OFFSET);
+  timer.every(250, showPedalReadings);
 }
 
-void loop() {
-  time = millis();
-  if ((time - previous_time) > 1000) {
-    previous_time = time;
-    int32_t brake_pedal_reading = brake_pedal.readValue();
-    int32_t throttle_pedal_reading = throttle_pedal.readValue();
-    int32_t clutch_pedal_reading = clutch_pedal.readValue();
-    Serial.print("Brake:\t");
-    Serial.print(brake_pedal_reading);
-    Serial.print("\t Throttle:\t");
-    Serial.print(throttle_pedal_reading);
-    Serial.print("\t Clutch:\t");
-    Serial.println(clutch_pedal_reading);
-  }
+void loop() { timer.tick(); }
+
+bool showPedalReadings(void *) {
+  int32_t brake_pedal_reading = brake_pedal.readValue();
+  int32_t throttle_pedal_reading = throttle_pedal.readValue();
+  int32_t clutch_pedal_reading = clutch_pedal.readValue();
+  Serial.print("Brake:\t");
+  Serial.print(brake_pedal_reading);
+  Serial.print("\t Throttle:\t");
+  Serial.print(throttle_pedal_reading);
+  Serial.print("\t Clutch:\t");
+  Serial.println(clutch_pedal_reading);
+  return true;
 }
