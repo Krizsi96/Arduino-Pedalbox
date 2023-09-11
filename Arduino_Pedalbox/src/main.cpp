8d5f0f0515f396295e575ac1c6295a58dace9a7f
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-09-11 19:23:09 +0100

    FEAT:: apply low pass filter on brake signal

--- Arduino_Pedalbox/src/main.cpp
+++ Arduino_Pedalbox/src/main.cpp
@@ -1,3 +1,4 @@
+#include <Filters.h>
 #include <arduino-timer.h>
 
 #include "ArduinoWrapper.hpp"
@@ -5,6 +6,8 @@
 #include "Pedal.hpp"
 #include "Potmeter.hpp"
 #include "SensorConfig.h"
+
+#define FILTER_FREQUENCY 10
 
 ArduinoWrapper arduino;
 
@@ -18,6 +21,8 @@ Pedal brake_pedal(&brake_load_cell);
 Pedal throttle_pedal(&throttle_potmeter);
 Pedal clutch_pedal(&clutch_potmeter);
 
+FilterOnePole brake_filter_lowpass(LOWPASS, FILTER_FREQUENCY);
+
 bool showPedalReadings(void *);
 
 void setup() {
@@ -25,7 +30,7 @@ void setup() {
   brake_pedal.setOffset(BRAKE_PEDAL_OFFSET);
   throttle_pedal.setOffset(THROTTLE_PEDAL_OFFSET);
   clutch_pedal.setOffset(CLUTCH_PEDAL_OFFSET);
-  timer.every(250, showPedalReadings);
+  timer.every(1, showPedalReadings);
 }
 
 void loop() { timer.tick(); }
@@ -34,11 +39,16 @@ bool showPedalReadings(void *) {
   int32_t brake_pedal_reading = brake_pedal.readValue();
   int32_t throttle_pedal_reading = throttle_pedal.readValue();
   int32_t clutch_pedal_reading = clutch_pedal.readValue();
-  Serial.print("Brake:\t");
-  Serial.print(brake_pedal_reading);
-  Serial.print("\t Throttle:\t");
-  Serial.print(throttle_pedal_reading);
-  Serial.print("\t Clutch:\t");
+
+  brake_filter_lowpass.input(brake_pedal_reading);
+
+  Serial.print(">brake:");
+  Serial.println(brake_pedal_reading);
+  Serial.print(">brake_filter:");
+  Serial.println(brake_filter_lowpass.output());
+  Serial.print(">throttle:");
+  Serial.println(throttle_pedal_reading);
+  Serial.print(">clutch:");
   Serial.println(clutch_pedal_reading);
   return true;
 }
