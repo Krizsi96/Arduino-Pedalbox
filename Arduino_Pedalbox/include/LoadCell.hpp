fe60bc85882ff718a1f89c23a49e3a67a87a8b52
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-09-12 19:24:18 +0100

    FEAT:: implementing the signal filtering in the load cell class

--- Arduino_Pedalbox/include/LoadCell.hpp
+++ Arduino_Pedalbox/include/LoadCell.hpp
@@ -1,6 +1,7 @@
 #ifndef _LOADCELL_HPP_
 #define _LOADCELL_HPP_
 
+#include <Filters.h>
 #include <HX711.h>
 
 #include "SensorInterface.hpp"
@@ -11,9 +12,14 @@ class LoadCell : public SensorInterface {
   ~LoadCell() override = default;
   int32_t getReadingValue() override;
   void setOffset(int32_t new_offset) override;
+  void setFilter(FILTER_TYPE filter_type, float cut_off_frequency,
+                 float initial_value);
+  int32_t getRawReadingValue();
 
  private:
   HX711 load_cell_;
+  FilterOnePole filter_;
+  int32_t applyFilterTo(int32_t raw_reading);
 };
 
 #endif  //_LOADCELL_HPP_
--- Arduino_Pedalbox/src/LoadCell.cpp
+++ Arduino_Pedalbox/src/LoadCell.cpp
@@ -5,11 +5,27 @@ LoadCell::LoadCell(byte serial_clock_pin_init, byte serial_data_pin_init) {
 }
 
 int32_t LoadCell::getReadingValue() {
-  int32_t reading = load_cell_.get_value();
+  int32_t raw_reading = getRawReadingValue();
+  int32_t reading = applyFilterTo(raw_reading);
   return reading;
 }
 
 void LoadCell::setOffset(int32_t new_offset) {
   int32_t offset_to_substract = new_offset * -1;
   load_cell_.set_offset(offset_to_substract);
+}
+
+void LoadCell::setFilter(FILTER_TYPE filter_type, float cut_off_frequency,
+                         float initial_value) {
+  filter_.setFilter(filter_type, cut_off_frequency, initial_value);
+}
+
+int32_t LoadCell::applyFilterTo(int32_t raw_reading) {
+  int32_t filtered_reading = filter_.input(raw_reading);
+  return filtered_reading;
+}
+
+int32_t LoadCell::getRawReadingValue() {
+  int32_t reading = load_cell_.get_value();
+  return reading;
 }
