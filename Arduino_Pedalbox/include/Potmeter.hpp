d39aade61dcdc89c2a6c13880c3d10e9fbd4e885
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-09-12 19:40:48 +0100

    FEAT:: implementing the signal filtering in the potmeter class

--- Arduino_Pedalbox/include/Potmeter.hpp
+++ Arduino_Pedalbox/include/Potmeter.hpp
@@ -1,6 +1,8 @@
 #ifndef _POTMETER_HPP_
 #define _POTMETER_HPP_
 
+#include <Filters.h>
+
 #include "SensorInterface.hpp"
 
 class Potmeter : public SensorInterface {
@@ -9,10 +11,15 @@ class Potmeter : public SensorInterface {
   ~Potmeter() override = default;
   int32_t getReadingValue() override;
   void setOffset(int32_t new_offset) override;
+  void setFilter(FILTER_TYPE filter_type, float cut_off_frequency,
+                 float initial_value);
+  int32_t getRawReadingValue();
 
  private:
   uint8_t analog_pin_;
   int32_t offset_;
+  FilterOnePole filter_;
+  int32_t applyFilterTo(int32_t raw_reading);
 };
 
 #endif  //_POTMETER_HPP_
--- Arduino_Pedalbox/src/Potmeter.cpp
+++ Arduino_Pedalbox/src/Potmeter.cpp
@@ -6,7 +6,23 @@ Potmeter::Potmeter(uint8_t analog_pin_init)
     : analog_pin_(analog_pin_init), offset_(0) {}
 
 int32_t Potmeter::getReadingValue() {
-  return analogRead(analog_pin_) + offset_;
+  int32_t raw_reading = getRawReadingValue();
+  int32_t reading = applyFilterTo(raw_reading);
+  return reading;
 }
 
 void Potmeter::setOffset(int32_t new_offset) { offset_ = new_offset; }
+
+void Potmeter::setFilter(FILTER_TYPE filter_type, float cut_off_frequency,
+                         float initial_value) {
+  filter_.setFilter(filter_type, cut_off_frequency, initial_value);
+}
+
+int32_t Potmeter::applyFilterTo(int32_t raw_reading) {
+  int32_t filtered_reading = filter_.input(raw_reading);
+  return filtered_reading;
+}
+
+int32_t Potmeter::getRawReadingValue() {
+  return analogRead(analog_pin_) + offset_;
+}
