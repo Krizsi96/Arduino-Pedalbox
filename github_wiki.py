5feccc46140d57b29fd3e7315456afc171f5fbae
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-10-09 20:37:55 +0200

    FEAT:: check if the wiki repo is up to date

--- github_wiki.py
+++ github_wiki.py
@@ -16,3 +16,12 @@ if not os.path.isdir('Arduino-Pedalbox.wiki'):
         sys.exit(1)
 
 print(f"{GREEN}Arduino-Pedalbox.wiki is cloned{NC}")
+
+# Check if the wiki of the project is up to date and pull it if not
+os.chdir('Arduino-Pedalbox.wiki')
+result = subprocess.run(['git', 'pull'])
+if result.returncode != 0:
+    print(f"{RED}Arduino-Pedalbox.wiki could not be pulled{NC}")
+    sys.exit(1)
+
+print(f"{GREEN}Arduino-Pedalbox.wiki is up to date{NC}")
