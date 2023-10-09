99aeb3cfadbffe3472fcbd3a8c0b2fdd1fff9939
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-10-09 20:33:50 +0200

    FEAT:: clone the wiki repo and check if it was successful

--- github_wiki.py
+++ github_wiki.py
@@ -6,9 +6,13 @@ GREEN = '\033[32m'
 RED = '\033[31m'
 NC = '\033[0m'
 
-# Check if the wiki of the project is cloned
+# Check if the wiki of the project is cloned and clone it if not
 if not os.path.isdir('Arduino-Pedalbox.wiki'):
     print(f"{RED}Arduino-Pedalbox.wiki is not cloned{NC}")
-    sys.exit(1)
-else:
-    print(f"{GREEN}Arduino-Pedalbox.wiki is cloned{NC}")
+    print("Cloning Arduino-Pedalbox.wiki...")
+    result = subprocess.run(['git', 'clone', 'https://github.com/Krizsi96/Arduino-Pedalbox.wiki.git'])
+    if result.returncode != 0:
+        print(f"{RED}Arduino-Pedalbox.wiki could not be cloned{NC}")
+        sys.exit(1)
+
+print(f"{GREEN}Arduino-Pedalbox.wiki is cloned{NC}")
