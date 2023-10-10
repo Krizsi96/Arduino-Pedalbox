8fee68c2aa12914a45b21507e316a98828f6e52e
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-10-10 19:52:13 +0100

    REFACTOR:: update dependency path handling

--- build_UML.py
+++ build_UML.py
@@ -2,7 +2,7 @@
 import os
 import subprocess
 import sys
-sys.path.append("../UML_code_generator")
+sys.path.append("StarUML-code-generator")
 from code_postprocessing import file
 import click
 
@@ -19,6 +19,7 @@ def cli():
 @click.argument('package', default=PACKAGE_NAME, required=False)
 @click.option('--with-diagrams', '-d', is_flag=True, help="Export diagrams also")
 def build_uml(package, with_diagrams):
+    checkDependecies()
     if with_diagrams:
         generateDiagrams()
     generateHeaderFiles(package)
@@ -28,23 +29,24 @@ def build_uml(package, with_diagrams):
 
 @click.command(help="export diagrams from UML models")
 def export_diagrams():
+    checkDependecies()
     generateDiagrams()
 
 def generateDiagrams():
     click.echo(click.style("generate diagrams form UML model", fg='cyan'))
     subprocess.run(['staruml', 'image', SOURCE_MODEL, '-f', 'svg', '-o', f'{DIAGRAM_OUTPUT_DIR}<%=filenamify(element.name)%>_diagram.svg'])
     current_directory = os.getcwd()
-    subprocess.run(['python3', '../UML_code_generator/svg_postprocess.py', f'{current_directory}/{DIAGRAM_OUTPUT_DIR}'])
+    subprocess.run(['python3', 'StarUML-code-generator/svg_postprocess.py', f'{current_directory}/{DIAGRAM_OUTPUT_DIR}'])
     click.echo()
 
 def generateHeaderFiles(package):
     click.echo(click.style("generate header files from UML classes", fg='cyan'))
-    subprocess.run(['staruml', 'ejs', SOURCE_MODEL, '-t', '../UML_code_generator/.ejs/cpp-class.ejs', '-s', f'{package}::@UMLClass', '-o', 'Arduino_Pedalbox/include/<%=filenamify(element.name)%>.hpp'])
+    subprocess.run(['staruml', 'ejs', SOURCE_MODEL, '-t', 'StarUML-code-generator/.ejs/cpp-class.ejs', '-s', f'{package}::@UMLClass', '-o', 'Arduino_Pedalbox/include/<%=filenamify(element.name)%>.hpp'])
     click.echo()
 
 def generateInterfaceDefinitions(package):
     click.echo(click.style("generate header files from UML interfaces", fg='cyan'))
-    subprocess.run(['staruml', 'ejs', SOURCE_MODEL, '-t', '../UML_code_generator/.ejs/cpp-class.ejs', '-s', f'{package}::@UMLInterface', '-o', 'Arduino_Pedalbox/include/<%=filenamify(element.name)%>.hpp'])
+    subprocess.run(['staruml', 'ejs', SOURCE_MODEL, '-t', 'StarUML-code-generator/.ejs/cpp-class.ejs', '-s', f'{package}::@UMLInterface', '-o', 'Arduino_Pedalbox/include/<%=filenamify(element.name)%>.hpp'])
 
 def formatHeaderFiles():
     click.echo(click.style("format header files with clang (google style)", fg='cyan'))
@@ -66,6 +68,25 @@ def postProcessCode():
     PedalboxHPP = file("Arduino_Pedalbox/include/Pedalbox.hpp")
     PedalboxHPP.replacePart("\"Joystick.hpp\"", "<Joystick.h>")
     click.echo()
+
+def checkDependecies():
+    click.echo(click.style("check dependencies", fg='cyan'))
+    if not os.path.exists('StarUML-code-generator'):
+        click.echo(click.style("clone StarUML-code-generator", fg='cyan'))
+        result = subprocess.run(['git', 'clone', 'https://github.com/Krizsi96/StarUML-code-generator.git'])
+        if result.returncode != 0:
+            click.echo(click.style(f"StarUML-code-generator could not be cloned", fg="red"))
+            sys.exit(1)
+        click.echo(click.style(f"StarUML-code-generator is cloned", fg="green"))
+    result = subprocess.run(['git', 'checkout', 'main'], cwd='StarUML-code-generator')
+    if result.returncode != 0:
+        click.echo(click.style(f"Could not checkout 'main'", fg="red"))
+        sys.exit(1)
+    result = subprocess.run(['git', 'pull'], cwd='StarUML-code-generator')
+    if result.returncode != 0:
+        click.echo(click.style(f"Could not pull", fg="red"))
+        sys.exit(1)
+    click.echo(click.style(f"StarUML-code-generator is up to date", fg="green")) 
 
 cli.add_command(build_uml)
 cli.add_command(export_diagrams)
